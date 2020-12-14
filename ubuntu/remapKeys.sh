4b8da0d6699c148a99b0a46b2ee3199e6606c8c2
Viktor Zhurbin <vzhurbin@gmail.com>
2020-12-14 12:24:37 +0300

    up

--- ubuntu/remapKeys.sh
+++ ubuntu/remapKeys.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Swap PgUp/PgDown and Home/End keys respectively
-# Make file executable and add as a script in Startup Applications
+# Make file executable (chmod +x /path/to/your/file.txt)
+# and add it as a script in Startup Applications
 xmodmap -e "keycode 112 = Home"
 xmodmap -e "keycode 117 = End"
 xmodmap -e "keycode 110 = Prior"
